e9daea091f09e797d27d6db159c41d9bd8409734
Paige Power <paige.power2@gmail.com>
2018-05-04 18:54:22 -0400

    added more comments in JS

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -11,20 +11,24 @@ let checkButton = document.getElementById("checkPalindrome");
 function isPalindrome(string) {
     // regex matches a to z, 0 to 9 matches any digit, i in regex indicates case insensitivity 
     string = string.replace(/[^a-z0-9]+/gi, "").toLowerCase();
+    // split string into array, reverse array and join 
     let reversedString = string.split("").reverse().join("");
     // triple equal sign, strict equality operator 
     // return boolean
     return string === reversedString;
 }
 
-// on click run function to check if user input is palindrome
+// on click run function to check if user input is palindrome and notify user if successful or unsuccessful
 checkButton.addEventListener("click", function () {
     let value = document.getElementById("inputPalindrome").value;
     let notification = document.getElementById("notification");
 
+    // if user input is palindrome, alert them 
     if (isPalindrome(value)) {
         notification.innerHTML = "👍 It's a palindrome! 👍";
         notification.className = "alert alertSuccess";
+
+    // else user input is not a palindrome , alert them 
     } else {
         notification.innerHTML = "👎 Not a palindrome! Try again! 👎";
         notification.className = "alert alertDanger";
