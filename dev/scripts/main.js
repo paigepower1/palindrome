2ea95d2d0090f3141ad80f2fb525e289036ccc46
Paige Power <paige.power2@gmail.com>
2018-05-04 16:21:07 -0400

    font styling and centering

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,31 @@
-const ohHey = "Hello World";
+// vanilla JS palindrome checker
 
-console.log(ohHey);
+// pseudo code 
+// allow user to type any palindrome
+// user clicks button to test if it is a palindrome
+// alert user if they are successful or unsuccessful in creating a palindrome
+
+
+let checkButton = document.getElementById("checkPalindrome");
+
+function isPalindrome(string) {
+    // regex matches a to z, 0 to 9 matches any digit, i in regex indicates case insensitivity 
+    string = string.replace(/[^a-z0-9]+/gi, "").toLowerCase();
+    let reversedString = string.split("").reverse().join("");
+    // triple equal sign, strict equality operator 
+    // return boolean
+    return string === reversedString;
+}
+
+checkButton.addEventListener("click", function () {
+    let value = document.getElementById("inputPalindrome").value;
+    let notification = document.getElementById("notification");
+
+    if (isPalindrome(value)) {
+        notification.innerHTML = "It's a palindrome!";
+        notification.className = "alert alert-success";
+    } else {
+        notification.innerHTML = "That's not a palindrome!";
+        notification.className = "alert alert-danger";
+    }
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,33 @@
 "use strict";
 
-var ohHey = "Hello World";
+// vanilla JS palindrome checker
 
-console.log(ohHey);
+// pseudo code 
+// allow user to type any palindrome
+// user clicks button to test if it is a palindrome
+// alert user if they are successful or unsuccessful in creating a palindrome
+
+
+var checkButton = document.getElementById("checkPalindrome");
+
+function isPalindrome(string) {
+    // regex matches a to z, 0 to 9 matches any digit, i in regex indicates case insensitivity 
+    string = string.replace(/[^a-z0-9]+/gi, "").toLowerCase();
+    var reversedString = string.split("").reverse().join("");
+    // triple equal sign, strict equality operator 
+    // return boolean
+    return string === reversedString;
+}
+
+checkButton.addEventListener("click", function () {
+    var value = document.getElementById("inputPalindrome").value;
+    var notification = document.getElementById("notification");
+
+    if (isPalindrome(value)) {
+        notification.innerHTML = "It's a palindrome!";
+        notification.className = "alert alert-success";
+    } else {
+        notification.innerHTML = "That's not a palindrome!";
+        notification.className = "alert alert-danger";
+    }
+});
